f46fed6a009685b4fff551ed7b20f2153176bb70
Alex Glancevs <alex2531458@gmail.com>
2024-06-20 21:26:25 +0300

    feat: implement logic for handling click events from Button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,90 @@
-import './styles/App.scss';
+import { useState } from 'react';
 import Button from './components/Button';
+import Display from './components/Display';
+import './styles/App.scss';
 
 const App = () => {
+    const [result, setResult] = useState('0');
+    const [formula, setFormula] = useState('');
+
+    const calculateResult = (): void => { }
+
+    const handleOperator = (textContent: string): void => { }
+
+    const handleNumber = (textContent: string): void => { }
+
+    const handleClick = (id: string, textContent: string): void => {
+        switch (id) {
+            case 'one':
+            case 'two':
+            case 'three':
+            case 'four':
+            case 'five':
+            case 'six':
+            case 'seven':
+            case 'eight':
+            case 'nine':
+            case 'zero':
+                handleNumber(textContent);
+                break;
+            case 'clear':
+                setResult('0');
+                setFormula('');
+                break;
+            case 'negate':
+
+                break;
+            case 'percent':
+
+                break;
+            case 'divide':
+            case 'multiply':
+            case 'subtract':
+            case 'add':
+                handleOperator(textContent);
+                break;            
+            case 'decimal':
+                if (result.includes('.')) {
+                    return;
+                } else {
+                    setResult(prevResult => prevResult += '.');
+                    setFormula(prevValue => prevValue += '.');
+                }
+                break;            
+            case 'equals':
+                calculateResult();
+                break;
+        }
+    }
+
     return (
         <main className='calculator'>
+            <Display output={result} formula={formula} />
             <div className='calculator__buttons'>
                 {/* 1st row */}
-                <Button btnText='AC' btnFunction={() => {}} btnId='clear' btnType='actions'/>
-                <Button btnText='+/-' btnFunction={() => {}} btnId='negate' btnType='actions'/>
-                <Button btnText='%' btnFunction={() => {}} btnId='percent' btnType='actions'/>
-                <Button btnText='÷' btnFunction={() => {}} btnId='divide' btnType='fun'/>
+                <Button btnText='AC' btnFunction={handleClick} btnId='clear' btnType='actions'/>
+                <Button btnText='+/-' btnFunction={handleClick} btnId='negate' btnType='actions'/>
+                <Button btnText='%' btnFunction={handleClick} btnId='percent' btnType='actions'/>
+                <Button btnText='÷' btnFunction={handleClick} btnId='divide' btnType='fun'/>
                 {/* 2nd row */}
-                <Button btnText='7' btnFunction={() => {}} btnId='seven' />
-                <Button btnText='8' btnFunction={() => {}} btnId='eight' />
-                <Button btnText='9' btnFunction={() => {}} btnId='nine' />
-                <Button btnText='×' btnFunction={() => {}} btnId='multiply' btnType='fun'/>
+                <Button btnText='7' btnFunction={handleClick} btnId='seven' />
+                <Button btnText='8' btnFunction={handleClick} btnId='eight' />
+                <Button btnText='9' btnFunction={handleClick} btnId='nine' />
+                <Button btnText='×' btnFunction={handleClick} btnId='multiply' btnType='fun'/>
                 {/* 3rd row */}
-                <Button btnText='4' btnFunction={() => {}} btnId='four' />
-                <Button btnText='5' btnFunction={() => {}} btnId='five' />
-                <Button btnText='6' btnFunction={() => {}} btnId='six' />
-                <Button btnText='−' btnFunction={() => {}} btnId='subtract' btnType='fun'/>
+                <Button btnText='4' btnFunction={handleClick} btnId='four' />
+                <Button btnText='5' btnFunction={handleClick} btnId='five' />
+                <Button btnText='6' btnFunction={handleClick} btnId='six' />
+                <Button btnText='−' btnFunction={handleClick} btnId='subtract' btnType='fun'/>
                 {/* 4th row */}
-                <Button btnText='1' btnFunction={() => {}} btnId='one' />
-                <Button btnText='2' btnFunction={() => {}} btnId='two' />
-                <Button btnText='3' btnFunction={() => {}} btnId='three' />
-                <Button btnText='+' btnFunction={() => {}} btnId='add' btnType='fun'/>
+                <Button btnText='1' btnFunction={handleClick} btnId='one' />
+                <Button btnText='2' btnFunction={handleClick} btnId='two' />
+                <Button btnText='3' btnFunction={handleClick} btnId='three' />
+                <Button btnText='+' btnFunction={handleClick} btnId='add' btnType='fun'/>
                 {/* 5th row */}
-                <Button btnText='0' btnFunction={() => {}} btnId='zero' style='pill'/>
-                <Button btnText='.' btnFunction={() => {}} btnId='decimal' />
-                <Button btnText='=' btnFunction={() => {}} btnId='equals' btnType='fun'/>
+                <Button btnText='0' btnFunction={handleClick} btnId='zero' style='pill'/>
+                <Button btnText='.' btnFunction={handleClick} btnId='decimal' />
+                <Button btnText='=' btnFunction={handleClick} btnId='equals' btnType='fun'/>
             </div>
         </main>
     )
